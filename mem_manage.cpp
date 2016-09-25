163e0286066a56caafc76fa0e3536aec3f10a8b7
WeichenXu <weichenxu1993@icloud.com>
2016-09-25 10:24:44 -0400

    init data structure, ctr and des for the mem_analysis_sys

--- mem_manage.cpp
+++ mem_manage.cpp
@@ -1,6 +1,54 @@
-#include <iostream>
-#include <std.h>
+#include <mem_manage.h>
+
+// Implementation for the mem_manage
+// Author: Weichen
+// Date:09/24/2016
 
 namespace Memory_System{
+    // initialize static member for mem_analysis_sys
+    mem_analysis_sys::sys_instance_ = nullptr;
+    
+    // constructors and destructor sfor mem_analysis_sys
+    static mem_analysis_sys& mem_analysis_sys::getMemAnalysisSys()
+    {
+        if (sys_instance_ == nullptr){
+            sys_instance_ = new mem_analysis_sys();
+        }
+        return *sys_instance_;
+    }
+    mem_analysis_sys::mem_analysis_sys(size_t mem_access_times):
+        mem_access_times_(mem_access_times)
+    {
+        params_ = new parameters;
+        params_->mem_access_times_ = mem_access_times;
+        pages_.resize(mem_access_times);
+    }
+    ~mem_analysis_sys::mem_analysis_sys(){
+        pages_.clear();
+        if (sys_instance_ != nullptr){
+            delete sys_instance_˜;
+        }
+        if (params_ != nullptr){
+            delete params_;
+        }
+    }
+    
+    // shrink resident set
+    // 1. discard the pages whose use-bit == 0
+    // 2. set remaining pages use-bit == 0
+    void shrinkResidentSet(){
+        for (auto &page in pages_){
+            if (page.use_bit){
+                page.use_bit = 0;
+            }
+            else{
+                if (page.in_memory){
+                    --res_mem_size_;
+                    page.in_memory = false;
+                }
+            }
+        }
+    }
+
 
 }
--- mem_manage.h
+++ mem_manage.h
@@ -1,43 +1,63 @@
+#ifndef _mem_analysis_sys_
+#define _mem_analysis_sys_
 // Author: Weichen Xu
 // Date: 09/24/2016
 // Transplant memory management from .c to .cpp 
 
 #include <iostream>
 #include <std.h>
+#include <vector>
 
 namespace Memory_System{
+    // enum class for 
+
     class mem_analysis_sys{
     public:
         // use Singleton design pattern
-        static mem_analysis_sys& getMemAnalysisSys()
-        {
-            if (sys_instance_ == nullptr){
-                sys_instance_ = new mem_analysis_sys;
-            }
-            return *sys_instance_;
-        }
+        static mem_analysis_sys& getMemAnalysisSys();
 
         // getter and setter for the parameters
-        size_t getPageNum() const;
-        size_t getMemAccTimes() const;
-        size_t getPageFaultTimes() const;
+        size_t getPageNum() const { returnn this->params_->page_num; }
+        size_t getMemAccTimes() const { return this->params_->mem_acc_times; }
+        size_t getPageFaultTimes() const { return this->params_->page_fault_times; }
+        size_t getResidentSize() const { return this->res_mem_size_; }
 
         //----------------Utility function----------------//
         void shrinkResidentSize();
+        void memAccess();    
 
         //----------------Perform memory resident algorithm----------//
         // PFF algorithm
-        // 
+        void PFF();
+        // VSWS algorithm
+        void VSWS();
 
     private:
+        // constructor and destructor
+        mem_analysis_sys(int mem_access_times = 0);
+        ~mem_analysis_sys();
+
         static mem_analysis_sys* sys_instance_;
         // use pimpl pattern
-        paraters *params_(nullptr);
+
+        parameters* params_(nullptr);
         // nested class, parameters for mem_analysis_sys
         struct parameters{
             size_t page_num(0);
             size_t mem_acc_times(0);
             size_t page_fault_times(0);
         }
+        
+        // nested class, pages in memory
+        struct page{
+            size_t address(0);
+            bool in_memory(false);
+            int use_bit(0);
+        }
+
+        // resident memory
+        std::vector<page> pages_;
+        size_t res_mem_size_(0);
     };
 }
+#endif
